683bf52999140630c6d10511637437dfa652417e
eaCe <webmaster@slaps.de>
2022-04-16 14:24:44 +0200

    feat: add simple filter

---
+++ fragments/filter-form.php
@@ -0,0 +1,48 @@
+<?php
+$sql = \rex_sql::factory();
+$causers = $sql->getArray('SELECT causer_id FROM ' . \rex::getTable('activity_log') . ' WHERE causer_id  IS NOT NULL GROUP BY causer_id ORDER BY causer_id');
+?>
+
+<form action="<?=rex_url::currentBackendPage()?>" class="form-inline">
+    <div class="form-horizontal" style="text-align: right">
+        <div class="form-group" style="text-align: left">
+            <label class="control-label col-sm-3" for="filter_type">Type</label>
+            <div class="col-sm-9">
+                <select class="form-control" name="type" id="filter_type">
+                    <option value="" <?=$this->type === '' ? 'selected' : ''?>>-</option>
+                    <option <?=$this->type === rex_activity::TYPE_ADD ? 'selected' : ''?> value="<?=rex_activity::TYPE_ADD?>"><?=rex_activity::TYPE_ADD?></option>
+                    <option <?=$this->type === rex_activity::TYPE_UPDATE ? 'selected' : ''?> value="<?=rex_activity::TYPE_UPDATE?>"><?=rex_activity::TYPE_UPDATE?></option>
+                    <option <?=$this->type === rex_activity::TYPE_DELETE ? 'selected' : ''?> value="<?=rex_activity::TYPE_DELETE?>"><?=rex_activity::TYPE_DELETE?></option>
+                    <option <?=$this->type === rex_activity::TYPE_EDIT ? 'selected' : ''?> value="<?=rex_activity::TYPE_EDIT?>"><?=rex_activity::TYPE_EDIT?></option>
+                    <option <?=$this->type === rex_activity::TYPE_INFO ? 'selected' : ''?> value="<?=rex_activity::TYPE_INFO?>"><?=rex_activity::TYPE_INFO?></option>
+                    <option <?=$this->type === rex_activity::TYPE_WARNING ? 'selected' : ''?> value="<?=rex_activity::TYPE_WARNING?>"><?=rex_activity::TYPE_WARNING?></option>
+                    <option <?=$this->type === rex_activity::TYPE_ERROR ? 'selected' : ''?> value="<?=rex_activity::TYPE_ERROR?>"><?=rex_activity::TYPE_ERROR?></option>
+                    <option <?=$this->type === rex_activity::TYPE_CRITICAL ? 'selected' : ''?> value="<?=rex_activity::TYPE_CRITICAL?>"><?=rex_activity::TYPE_CRITICAL?></option>
+                    <option <?=$this->type === rex_activity::TYPE_NOTICE ? 'selected' : ''?> value="<?=rex_activity::TYPE_NOTICE?>"><?=rex_activity::TYPE_NOTICE?></option>
+                    <option <?=$this->type === rex_activity::TYPE_DEBUG ? 'selected' : ''?> value="<?=rex_activity::TYPE_DEBUG?>"><?=rex_activity::TYPE_DEBUG?></option>
+                </select>
+            </div>
+        </div>
+
+        <?php if ($causers) : ?>
+            <div class="form-group" style="text-align: left">
+                <label class="control-label col-sm-3" for="filter_user">User</label>
+                <div class="col-sm-9">
+                    <select class="form-control" name="user" id="filter_user">
+                        <option value="" <?=$this->user === '' ? 'selected' : ''?>>-</option>
+                        <?php foreach ($causers as $causer) : ?>
+                            <option <?=$this->user == $causer['causer_id'] ? 'selected' : ''?> value="<?=$causer['causer_id']?>"><?=\rex_user::get($causer['causer_id'])->getName()?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+            </div>
+        <?php endif; ?>
+
+        <div class="form-group" style="text-align: left">
+            <div class="col-sm-12">
+                <button type="submit" class="btn btn-default" name="filter" value="1">Filter</button>
+            </div>
+        </div>
+    </div>
+</form>
+<hr style="margin-top: 15px; margin-bottom: 0;">
--- pages/system.activity-log.php
+++ pages/system.activity-log.php
@@ -1,27 +1,38 @@
 <?php
 $table = \rex::getTable('activity_log');
 
-if(rex_post('delete_old_logs') && rex_post('delete_old_logs') == 1) {
-    $sql = rex_sql::factory();
+if (\rex_post('delete_old_logs') && \rex_post('delete_old_logs') == 1) {
+    $sql = \rex_sql::factory();
     $sql->setTable($table);
     $sql->setWhere('created_at < now() - interval 7 day');
     $sql->delete();
 }
 
-if(rex_post('delete_all_logs') && rex_post('delete_all_logs') == 1) {
-    $sql = rex_sql::factory();
+if (\rex_post('delete_all_logs') && \rex_post('delete_all_logs') == 1) {
+    $sql = \rex_sql::factory();
     $sql->setTable($table);
     $sql->delete();
 }
 
-$addon = rex_addon::get('activity_log');
-$types = rex_get('type');
+$addon = \rex_addon::get('activity_log');
+$type = \rex_get('type');
+$user = \rex_get('user');
 
 $query = 'SELECT created_at,type,message,causer_id FROM ' . $table;
+$where = [];
 
-if ($types !== '' && is_array($types)) {
-    $query .= ' WHERE FIND_IN_SET(type, "' . implode(',', rex_get('type')) . '")';
+if ($type && $type !== '') {
+    $where[] = 'type = "' . $type . '"';
 }
+
+if ($user && $user !== '') {
+    $where[] = 'causer_id = ' . $user;
+}
+
+if ($where) {
+    $query .= ' WHERE ' . implode(' AND ', $where);
+}
+
 $query .= ' ORDER BY created_at DESC';
 
 $list = \rex_list::factory($query, 100, 'rex_activity');
@@ -39,26 +50,19 @@ $list->setColumnFormat('causer_id', 'custom', 'rex_activity::userListCallback');
 $list->setColumnFormat('message', 'custom', 'rex_activity::messageListCallback');
 $list->setColumnFormat('type', 'custom', 'rex_activity::typeListCallback');
 
-//$content = '<form action="' . rex_url::currentBackendPage() . '">
-//    <label class="checkbox-inline">
-//      <input name="type[]" type="checkbox" id="type_info" value="info" ' . (is_array($types) && is_numeric(array_search('info', $types)) ? "checked" : "") . '> info
-//    </label>
-//    <label class="checkbox-inline">
-//      <input name="type[]" type="checkbox" id="type_error" value="error" ' . (is_array($types) && is_numeric(array_search('error', $types)) ? "checked" : "") . '> error
-//    </label>
-//    <label class="checkbox-inline">
-//      <input name="type[]" type="checkbox" id="type_critical" value="critical" ' . (is_array($types) && is_numeric(array_search('critical', $types)) ? "checked" : "") . '> critical
-//    </label>
-//    <button type="submit" name="filter" value="1">Filter</button>
-//</form>';
 
-$content = $list->get();
+$filterFragment = new \rex_fragment();
+$filterFragment->setVar('type', $type);
+$filterFragment->setVar('user', $user);
+$content = $filterFragment->parse('filter-form.php');
 
-$content .= '<hr><form method="post" action="' . rex_url::currentBackendPage() . '" style="text-align: right">
-    <button type="submit" class="btn btn-danger" name="delete_old_logs" value="1">'.$addon->i18n('delete_older_than_7_days').'</button>
-    <button type="submit" class="btn btn-danger" name="delete_all_logs" value="1">'.$addon->i18n('delete_all').'</button>
+$content .= $list->get();
+
+$content .= '<hr><form method="post" action="' . \rex_url::currentBackendPage() . '" style="text-align: right">
+    <button type="submit" class="btn btn-danger" name="delete_old_logs" value="1">' . $addon->i18n('delete_older_than_7_days') . '</button>
+    <button type="submit" class="btn btn-danger" name="delete_all_logs" value="1">' . $addon->i18n('delete_all') . '</button>
 </form>';
 
-$fragment = new rex_fragment();
+$fragment = new \rex_fragment();
 $fragment->setVar('body', $content, false);
 echo $fragment->parse('core/page/section.php');
