f03c5f0db8671fd64b6d0cbfbf0f3db98d12a57b
eaCe <webmaster@slaps.de>
2022-04-16 08:19:26 +0200

    fix: get -> post

--- pages/system.activity-log.php
+++ pages/system.activity-log.php
@@ -1,14 +1,14 @@
 <?php
 $table = \rex::getTable('activity_log');
 
-if(rex_get('delete_old_logs') && rex_get('delete_old_logs') == 1) {
+if(rex_post('delete_old_logs') && rex_post('delete_old_logs') == 1) {
     $sql = rex_sql::factory();
     $sql->setTable($table);
     $sql->setWhere('created_at < now() - interval 7 day');
     $sql->delete();
 }
 
-if(rex_get('delete_all_logs') && rex_get('delete_all_logs') == 1) {
+if(rex_post('delete_all_logs') && rex_post('delete_all_logs') == 1) {
     $sql = rex_sql::factory();
     $sql->setTable($table);
     $sql->delete();
@@ -54,7 +54,7 @@ $list->setColumnFormat('type', 'custom', 'rex_activity::typeListCallback');
 
 $content = $list->get();
 
-$content .= '<hr><form action="' . rex_url::currentBackendPage() . '" style="text-align: right">
+$content .= '<hr><form method="post" action="' . rex_url::currentBackendPage() . '" style="text-align: right">
     <button type="submit" class="btn btn-danger" name="delete_old_logs" value="1">'.$addon->i18n('delete_older_than_7_days').'</button>
     <button type="submit" class="btn btn-danger" name="delete_all_logs" value="1">'.$addon->i18n('delete_all').'</button>
 </form>';
