edc6af986630fb57e2149ae5a75f6ed90ba6f0c9
eaCe <webmaster@slaps.de>
2022-09-10 14:07:42 +0200

    chore: try again...

--- .tools/bootstrap.php
+++ .tools/bootstrap.php
@@ -4,10 +4,10 @@ unset($REX);
 $REX['REDAXO'] = true;
 $REX['HTDOCS_PATH'] = '../../../../';
 $REX['BACKEND_FOLDER'] = 'redaxo';
-$REX['LOAD_PAGE'] = false;
+$REX['LOAD_PAGE'] = true;
 
-require __DIR__.'../../../core/boot.php';
-require __DIR__.'../../../core/packages.php';
+require __DIR__.'../../../../core/boot.php';
+require __DIR__.'../../../../core/packages.php';
 
 // use original error handlers of the tools
 rex_error_handler::unregister();
