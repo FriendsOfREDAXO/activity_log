9433ece4cbeeba00c94b85b5c4cf56c2a7a1655f
eaCe <webmaster@slaps.de>
2022-09-13 21:28:41 +0200

    chore: try playwright

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,7 @@ const config = {
   reporter: 'line',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: true,
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
