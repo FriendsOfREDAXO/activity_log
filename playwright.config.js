96b56bff2249921b654a13e0eb9614e6081c38e6
Thorben <webmaster@slaps.de>
2022-09-14 09:45:10 +0200

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,7 @@ const config = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'line',
+  reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     headless: true,
