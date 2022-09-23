77ea6fdb24acf6bb434279458c265b862d86a126
eaCe <webmaster@slaps.de>
2022-09-23 13:07:01 +0200

    chore: try to get screens to debug...

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -12,71 +12,71 @@
 //            |___/
 
 module.exports = {
-  // An array of folders (excluding subfolders) where your tests are located;
-  // if this is not specified, the test source must be passed as the second argument to the test runner.
-  src_folders: ['tests/e2e'],
+    // An array of folders (excluding subfolders) where your tests are located;
+    // if this is not specified, the test source must be passed as the second argument to the test runner.
+    src_folders: ['tests/e2e'],
 
-  // See https://nightwatchjs.org/guide/concepts/page-object-model.html
-  // page_objects_path: ['test/e2e/page-objects'],
+    // See https://nightwatchjs.org/guide/concepts/page-object-model.html
+    // page_objects_path: ['test/e2e/page-objects'],
 
-  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
-  // custom_commands_path: ['test/e2e/custom-commands'],
+    // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
+    // custom_commands_path: ['test/e2e/custom-commands'],
 
-  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-assertions.html
-  // custom_assertions_path: ['test/e2e/custom-assertions'],
+    // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-assertions.html
+    // custom_assertions_path: ['test/e2e/custom-assertions'],
 
-  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-plugins.html
-  // plugins: [],
+    // See https://nightwatchjs.org/guide/extending-nightwatch/adding-plugins.html
+    // plugins: [],
 
-  // See https://nightwatchjs.org/guide/concepts/test-globals.html
-  globals_path: '',
+    // See https://nightwatchjs.org/guide/concepts/test-globals.html
+    globals_path: '',
 
-  webdriver: {},
+    webdriver: {},
 
-  test_settings: {
-    default: {
-      disable_error_log: false,
-      launch_url: '${LAUNCH_URL}',
+    test_settings: {
+        default: {
+            disable_error_log: false,
+            launch_url: '${LAUNCH_URL}',
 
-      screenshots: {
-        enabled: false,
-        path: 'screens',
-        on_failure: true
-      },
+            screenshots: {
+                enabled: true,
+                on_failure: true,
+                path: "./screens"
+            },
 
-      desiredCapabilities: {
-        browserName: 'firefox'
-      },
+            desiredCapabilities: {
+                browserName: 'firefox'
+            },
 
-      webdriver: {
-        start_process: true,
-        server_path: ''
-      },
+            webdriver: {
+                start_process: true,
+                server_path: ''
+            },
 
-    },
+        },
 
-    firefox: {
-      desiredCapabilities: {
-        browserName: 'firefox',
-        alwaysMatch: {
-          acceptInsecureCerts: true,
-          'moz:firefoxOptions': {
-            args: [
-              // '-headless',
-              // '-verbose'
-            ]
-          }
-        }
-      },
-      webdriver: {
-        start_process: true,
-        server_path: '',
-        cli_args: [
-          // very verbose geckodriver logs
-          // '-vv'
-        ]
-      }
-    },
+        firefox: {
+            desiredCapabilities: {
+                browserName: 'firefox',
+                alwaysMatch: {
+                    acceptInsecureCerts: true,
+                    'moz:firefoxOptions': {
+                        args: [
+                            // '-headless',
+                            // '-verbose'
+                        ]
+                    }
+                }
+            },
+            webdriver: {
+                start_process: true,
+                server_path: '',
+                cli_args: [
+                    // very verbose geckodriver logs
+                    // '-vv'
+                ]
+            }
+        },
 
-  }
+    }
 };
