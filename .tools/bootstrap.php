17dc2485bd09b2ad933d361a31cb1d639c5cecb3
Thomas Skerbis <thomas.skerbis@mac.com>
2026-08-11 16:55:34 +0200

    abhängigkeiten aktualisiert

--- .tools/bootstrap.php
+++ .tools/bootstrap.php
@@ -9,5 +9,9 @@ $REX['LOAD_PAGE'] = false;
 require __DIR__.'../../../../core/boot.php';
 require __DIR__.'../../../../core/packages.php';
 
+// In PHPUnit context, addon classes may not be autoloaded depending on setup state.
+require_once __DIR__.'/../lib/Activity.php';
+require_once __DIR__.'/../lib/rex_activity.php';
+
 // use original error handlers of the tools
 rex_error_handler::unregister();
